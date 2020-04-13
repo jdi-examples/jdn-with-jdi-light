040bf960ed15e173e7452c1d2241972d6502cf79
RomanIovlev <roman.iovlev.jdi@gmail.com>
2020-04-13 22:16:43 +0300

    remove page object

--- src/main/java/io/github/jditesting/entities/Logins.java
+++
@@ -1,8 +0,0 @@
-package io.github.jditesting.entities;
-
-import com.epam.jdi.tools.DataClass;
-
-public class Logins extends DataClass<Logins> {
-    public String name;
-    public String password;
-}
