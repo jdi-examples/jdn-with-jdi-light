ea45461c347fc680375519dabe91f4748e72b22f
RomanIovlev <roman.iovlev.jdi@gmail.com>
2020-04-13 22:35:20 +0300

    remove allure

--- src/test/java/io/github/epam/tests/example/SimpleTestsInit.java
+++ src/test/java/io/github/epam/tests/example/SimpleTestsInit.java
@@ -1,8 +1,10 @@
 package io.github.epam.tests.example;
 
 import io.github.jditesting.JdiTestingGithubSite;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import static com.epam.jdi.light.driver.WebDriverUtils.*;
 import static com.epam.jdi.light.elements.init.PageFactory.*;
 import static com.epam.jdi.light.logger.LogLevels.*;
 import static com.epam.jdi.light.settings.WebSettings.*;
@@ -14,5 +16,10 @@ public class SimpleTestsInit {
         logger.setLogLevel(STEP);
         initSite(JdiTestingGithubSite.class);
         logger.info("Run Tests");
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public static void tearDown() {
+        killAllSeleniumDrivers();
     }
 }
